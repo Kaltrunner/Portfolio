b9fa3bd93999d9223c9fd93e44589aff2c5f27e9
Erik Russell <erikrussells@gmail.com>
2023-03-02 12:45:33 -0500

    updated the margin

--- Desktop/Code Projects/erik-pro/src/Samples.js
+++ Desktop/Code Projects/erik-pro/src/Samples.js
@@ -14,13 +14,17 @@ function Samples() {
                 <img className="project-1" src="Screenshot 2023-02-21 at 11.13.26 AM.png" alt="green square project website"/>
                 </div></a>
                 
-                <div className="sample-work-example" >
-                    <img className="project-1" src="Screenshot 2023-02-21 at 11.54.40 AM.png" alt="" />
-                </div>
+                <a target="_blank" rel="noreferrer" href="https://github.com/Kaltrunner/Rudy" > <div className="sample-work-example" >
+                    <img className="project-1"  src="Screenshot 2023-02-21 at 11.54.40 AM.png" alt="" />
+                </div></a>
 
-                <div className="sample-work-example" >
+                <a target="_blank" rel="noreferrer" href="https://github.com/Kaltrunner/Snake-Pit-Game" > <div className="sample-work-example" >
                     <img className="project-1" src="Screenshot 2023-02-21 at 12.14.33 PM.png" alt=""/>
-                </div>
+                </div></a>
+
+                <a target="_blank" rel="noreferrer" href="https://github.com/Kaltrunner/design-system-sid.github.io" > <div className="sample-work-example" >
+                    <img className="project-1" src="Screenshot 2023-03-02 at 12.27.12 PM (2).png" alt=""/>
+                </div></a>
 
                 <div className="sample-work-example" >
                     <img className="project-1" src="Screenshot 2023-02-21 at 12.11.01 PM.png" alt=""/>
